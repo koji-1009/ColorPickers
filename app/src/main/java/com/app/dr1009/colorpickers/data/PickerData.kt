158448bed0b7238da4f45341ad59187272b085b7
Koji Wakamiya <koji.wakamiya@gmail.com>
2018-09-10 09:13:16 +0900

    Export String/Integer resource

--- app/src/main/java/com/app/dr1009/colorpickers/data/PickerData.kt
+++ app/src/main/java/com/app/dr1009/colorpickers/data/PickerData.kt
@@ -6,27 +6,6 @@ import android.databinding.Bindable
 class PickerData : BaseObservable() {
 
     @get:Bindable
-    val TYPE_R = "R"
-
-    @get:Bindable
-    val TYPE_G = "G"
-
-    @get:Bindable
-    val TYPE_B = "B"
-
-    @get:Bindable
-    val TYPE_A = "Alpha"
-
-    @get:Bindable
-    val TYPE_ROTATION = "Rotation"
-
-    @get:Bindable
-    val MAX_RGBA = 255
-
-    @get:Bindable
-    val MAX_ROTATION = 360
-
-    @get:Bindable
     val img = RGBAData(aDefault = 255, rDefault = 180, gDefault = 180, bDefault = 180)
 
     @get:Bindable
